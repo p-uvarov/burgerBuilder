9f8c4da0f928790576b65f62b327aec26742ce7c
Pavel Uvarov <pavel@uvarov.net>
2019-06-17 08:03:50 -0400

    Add unit tests and .htaccess

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -8,12 +8,21 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
-    it('Should render 3 navigation items if user is authenticated', () => {
-        const wrapper = shallow(<NavigationItems isAuth={true} />);
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    })
+
+    it('Should render 3 navigation items if user is authenticated.', () => {
+        wrapper.setProps({isAuth: true});
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
-    it('Should render 2 navigation items', () => {
-        const wrapper = shallow(<NavigationItems />);
+    it('Should render 2 navigation items if user is not authenticated.', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    })
+
+    it('Should consist logout navigation item if user is authenticated.', () => {
+        wrapper.setProps({isAuth: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Log out</NavigationItem>)).toEqual(true);
     })
 });
